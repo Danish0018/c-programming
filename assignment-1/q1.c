8333af7558f59760aa2217e4b45859967fcb7e44
Your Name <you@example.com>
2024-08-17 15:15:52 +0530

    sum

--- assignment-1/q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a,b,c;
-    a=4;
-    b=3;
-    c=a+b;
-    printf("sum = %d",c);
-    return 0;
-}
---
+++ assignment-1/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    long long num1 = 2147483600;
+    long long num2 = 100;
+    long long sum = num1 + num2;
+
+    printf("sum is %lld", sum);
+
+    return 0;
+}
